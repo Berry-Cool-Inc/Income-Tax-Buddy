c28bd120f8efd50fb4a4cec64f6e137d519c349e
FedoraTipper <FedoraTipper@users.noreply.github.com>
2019-09-19 19:48:26 +0200

    Restructure objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import IncomeTaxForm from './components/IncomeTaxForm';
+import IncomeTaxForm from './components/IncomeTax/IncomeTaxComponent';
 import './assets/App.css';
 import 'antd/dist/antd.css';
 
